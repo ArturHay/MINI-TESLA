717f9894cc50d929ee3800c11a58333ec6f882ef
mathixu <94596958+mathixu@users.noreply.github.com>
2021-12-01 17:05:07 +0100

    Add files via upload

---
+++ CLogsForLinux/main_LogsForLinux.cpp
@@ -0,0 +1,33 @@
+#include "cvehicule.h"
+
+
+int main()
+{
+
+    CLogs *v1 = new CLogs();
+
+    /*
+    addLogs(double p_mouvement, double p_vitesse, double p_duree,
+                            double p_latitude, double p_longitude, double p_state, double p_timer)
+    */
+
+    double fLogs[7][7] = {1.0};
+
+    double duree = 3.50;
+
+    for(int i=0; i<7; i++)
+    {
+            fLogs[i][0] = 1 * (i%6); // mvt
+            fLogs[i][1] = 25 * (i % 2); // vitesse
+            fLogs[i][2] += i*10; // Duree
+            fLogs[i][3] += i*5;
+            fLogs[i][4] = i%2;
+            fLogs[i][5] = duree * (i+1);
+            fLogs[i][6] = 10.0;
+            v1->addLogs(fLogs[i][0],fLogs[i][1],fLogs[i][2],fLogs[i][3],fLogs[i][4],fLogs[i][5],fLogs[i][6]);
+    }
+
+
+
+    return 0;
+}
