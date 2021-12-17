5754495ad7e137318e65aa7c368b492ae45fee10
ArturHay <78548668+ArturHay@users.noreply.github.com>
2021-12-17 15:31:34 +0100

    Update mainMaison.cpp

--- Cvehicule/mainMaison.cpp
+++ Cvehicule/mainMaison.cpp
@@ -3,30 +3,18 @@
 int main(int argc, char *argv[])
 {
 
-      CProximite *distance1=new CProximite();
-
-      Cvehicule *vehicule1=new Cvehicule();
-
-      /*vehicule1->arreter(3000);
+       Cvehicule *vehicule1=new Cvehicule();
 
           vehicule1->avancer(50,3600);
           vehicule1->tournerGuauche();
           vehicule1->avancer(50,6200);
           vehicule1->tournerGuauche();
-          vehicule1->avancer(50,4000);
+          vehicule1->avancer(50,4200);
           vehicule1->tournerGuauche();
           vehicule1->avancer(50,6200);
           vehicule1->tournerGuauche();
-         vehicule1->avancer(50,1600);*/
-
-
-
-         vehicule1->arreter(15000);
-         vehicule1->reculer(20, 5000);
-         vehicule1->demitourSlide();
-         vehicule1->avancer(60,5000);
-         vehicule1->arreter(250000);
-
+          vehicule1->avancer(50,1600);
 
     return (0);
 }
+
